61fe4e7ba612f48b83a2ed5da60963bb99a409e4
Milos <miloss.ilich@gmail.com>
2020-11-24 15:57:23 +0100

    Add Assigment 4 /pets/, add spring lifecycle control

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -12,6 +12,10 @@ public class SfgDiApplication {
 		ConfigurableApplicationContext configurableApplicationContext
 				= SpringApplication.run(SfgDiApplication.class, args);
 
+		PetController petController = configurableApplicationContext.getBean("petController", PetController.class);
+		System.out.println("--- The Best Pet is ---");
+		System.out.println(petController.whichPetIsTheBest());
+
 		System.out.println("--- Testing Profiles ----");
 		I18nController i18nController = (I18nController) configurableApplicationContext.getBean("i18nController");
 		System.out.println(i18nController.getText());
